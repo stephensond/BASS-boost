de920c78d661aad9d295fd7cd6b64849d63e6cc3
Ivan Rudyakov <rudyakov.i@northeastern.edu>
2023-02-11 16:46:27 -0500

    react bass boost implemention

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,77 @@
 import './App.css';
+import React from 'react';
 
-function addVideo() {
-  const selectedFile = document.getElementById('uploader').files[0];
-  console.log(selectedFile);
-  const objectURL = URL.createObjectURL(selectedFile);
-  const videoElement = document.createElement('video');
-  videoElement.src = objectURL;
-  videoElement.width="750";
-  videoElement.height="500";
-  videoElement.controls = true;
-  document.getElementById('head').appendChild(videoElement);
+class VideoComponent extends React.Component {
+
+  componentDidMount() {
+    const video = document.getElementById(this.props.id);
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const source = audioContext.createMediaElementSource(video);
+    const biquadFilter = audioContext.createBiquadFilter();
+
+    biquadFilter.type = "lowshelf";
+    biquadFilter.frequency.value = 1000;
+    biquadFilter.gain.value = 40;
+
+    source.connect(biquadFilter);
+    biquadFilter.connect(audioContext.destination);
+  }
+
+  playVideo = () => {
+    document.getElementById(this.props.id).play();
+  }
+
+  stopVideo = () => {
+    document.getElementById(this.props.id).pause();
+  }
+
+  render() {
+    return (
+      <div>
+        <video src={this.props.url} id={this.props.id} controls/>
+        <button onClick={this.playVideo}>PLAY</button>
+        <button onClick={this.stopVideo}>STOP</button>
+      </div>
+    );
+  }
 }
 
-function BASSBoost() {
-  const video = document.querySelector('video');
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const source = audioContext.createMediaElementSource(video);
-  const biquadFilter = audioContext.createBiquadFilter();
 
-  biquadFilter.type = "lowshelf";
-  biquadFilter.frequency.value = 1000;
-  biquadFilter.gain.value = 40;
+class App extends React.Component {
 
-  source.connect(biquadFilter);
-  biquadFilter.connect(audioContext.destination);
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoUrls: []
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
+  addVideo = () => {
+    const selectedFile = document.getElementById('uploader').files[0];
+    const objectURL = URL.createObjectURL(selectedFile);
+    this.setState((state, props) => ({
+      videoUrls: [...state.videoUrls, objectURL]
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header id="head" className="App-header">
-        Bass Boosting
-        <img alt="bass" src={"../bass.png"} />
-        <h1>React File Upload</h1>
+        <div>
+          <h1> BASS BOOST </h1>
+          <img src="../bassboost.gif"/>
+        </div>
         <input type="file" id="uploader" accept="video/*" />
-        <button onClick={addVideo}>Upload</button>
-        <button onClick={BASSBoost}>BASS BOOST</button>
+        <button onClick={this.addVideo}>BOOST</button>
+        {this.state.videoUrls.map((url, index) => (
+          <VideoComponent url={url} key={index} id={'video' + index}/>
+        ))}
       </header>
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
