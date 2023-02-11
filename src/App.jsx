5e21e2e29796b6fed913b40ff68816e6a91898d1
sho-r1024 <rakhangi.s@northeastern.edu>
2023-02-11 14:16:40 -0500

    what i have so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css';
+import './VideoModule.jsx'
+
+var moduleList = [];
 
 function addVideo() {
-  const selectedFile = document.getElementById('uploader').files[0];
-  console.log(selectedFile);
-  const objectURL = URL.createObjectURL(selectedFile);
-  const videoElement = document.createElement('video');
-  videoElement.src = objectURL;
-  videoElement.width="750";
-  videoElement.height="500";
-  videoElement.controls = true;
-  document.getElementById('head').appendChild(videoElement);
+  for(selectedFile in files) {
+    moduleList.push(selectedFile)
+    console.log(selectedFile);
+    const objectURL = URL.createObjectURL(selectedFile);
+    const videoElement = document.createElement('video');
+    videoElement.src = objectURL;
+    videoElement.width="750";
+    videoElement.height="500";
+    videoElement.controls = true;
+    document.getElementById('head').appendChild(videoElement);
+  }
+  // const selectedFile = document.getElementById('uploader').files[0];
+
 }
 
 function BASSBoost() {
@@ -19,23 +26,25 @@ function BASSBoost() {
   const biquadFilter = audioContext.createBiquadFilter();
 
   biquadFilter.type = "lowshelf";
-  biquadFilter.frequency.value = 1000;
-  biquadFilter.gain.value = 40;
+  biquadFilter.frequency.value = 10000;
+  biquadFilter.gain.value = 10;
 
   source.connect(biquadFilter);
   biquadFilter.connect(audioContext.destination);
 }
 
 function App() {
+
+
   return (
     <div className="App">
       <header id="head" className="App-header">
         Bass Boosting
         <img alt="bass" src={"../bass.png"} />
         <h1>React File Upload</h1>
-        <input type="file" id="uploader" accept="video/*" />
+        {/* <input type="file" id="uploader" accept="video/*" />
         <button onClick={addVideo}>Upload</button>
-        <button onClick={BASSBoost}>BASS BOOST</button>
+        <button onClick={BASSBoost}>BASS BOOST</button> */}
       </header>
     </div>
   );
---
+++ src/VideoModule.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './App.jsx';
+
+class videoModule extends React.Component {
+    render() {
+        <input type="file" id="uploader" accept="video/*" />;
+        <button onClick={addVideo}>Upload</button>;
+        <button onClick={BASSBoost}>BASS BOOST</button>;
+    }
+}
