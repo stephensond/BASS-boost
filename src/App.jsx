c8ab968faefd9854129ff84148bcc46d2fd2258d
David Stephenson <stephenson.d@northeastern.edu>
2023-02-10 23:02:37 -0500

    mess with frontend

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bass Boosting
+        <img alt="bass" src={"../bass.png"} />
+        <form>
+          <h1>React File Upload</h1>
+          <input type="file" />
+          <button type="submit">Upload</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
