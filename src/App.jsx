3b639fd29ee08553eed307def13a8223f6948902
Ivan Rudyakov <rudyakov.i@northeastern.edu>
2023-02-10 23:34:45 -0500

    chat gpt scuffed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
 import './App.css';
+import video from './movie.mp4';
+
+function BASSBoost() {
+  const video = document.querySelector('video');
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const source = audioContext.createMediaElementSource(video);
+  const biquadFilter = audioContext.createBiquadFilter();
+
+  biquadFilter.type = "lowshelf";
+  biquadFilter.frequency.value = 100;
+  biquadFilter.gain.value = 10;
+
+  source.connect(biquadFilter);
+  biquadFilter.connect(audioContext.destination);
+}
 
 function App() {
   return (
@@ -6,11 +21,13 @@ function App() {
       <header className="App-header">
         Bass Boosting
         <img alt="bass" src={"../bass.png"} />
+        <video className="video" src={video} width="750" height="500" controls></video>
         <form>
           <h1>React File Upload</h1>
           <input type="file" />
           <button type="submit">Upload</button>
         </form>
+        <button onClick={BASSBoost}/>
       </header>
     </div>
   );
