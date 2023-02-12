17be9f12092ae1111d3e73582a94520a110baf47
sho-r1024 <rakhangi.s@northeastern.edu>
2023-02-11 19:47:54 -0500

    reset index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import { Landing } from './Landing';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Landing />
+    <App />
   </React.StrictMode>
 );
 
